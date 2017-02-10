0ddd2f7c0e8ab5e65fba78415dcfc8476f78a79d
Mewen MICHEL <mewenmichel@yahoo.fr>
2017-02-10 23:33:51 +0100

    Added main screen

--- webclient/pages/hokuyo/hokuyo.js
+++ webclient/pages/hokuyo/hokuyo.js
@@ -1,12 +1,13 @@
 angular.module('app').controller('HokuyoCtrl', ['$rootScope', '$scope', 'Hokuyo',
 	function($rootScope, $scope, Hokuyo) {
 	$rootScope.act_page = 'hokuyo';
-	Hokuyo.displays.main = new HokuyoDisplay("mainHokDisp", "main");
 	Hokuyo.displays.one = new HokuyoDisplay("hok1", "one");
 	Hokuyo.displays.two = new HokuyoDisplay("hok2", "two");
+	Hokuyo.displays.main = new HokuyoDisplay("mainHokDisp", "main", Hokuyo.displays.one.dotColor, Hokuyo.displays.two.dotColor);
 	// $scope.test = "Coucou !";
 	// $scope.test3 = 42;
 
+/*
 	$scope.name = "hokuyo.polar_raw_data";
 	$scope.from = "hokuyo";
 	$scope.data = '{\n\
@@ -28,6 +29,50 @@ angular.module('app').controller('HokuyoCtrl', ['$rootScope', '$scope', 'Hokuyo'
 		[ 30, 235 ]\n\
 	]\n\
 }';
+*/
+
+	$scope.name = "lidar.all";
+	$scope.from = "lidar";
+	$scope.data = '{\n\
+ 	"hokuyos": [\n\
+	 	{\n\
+	 		"name": "one",\n\
+	 		"position": {\n\
+	 			"x": -6.2,\n\
+	 			"y": -6.2,\n\
+	 			"w": 0\n\
+	 		}\n\
+	 	},\n\
+	 	{\n\
+	 		"name": "two",\n\
+	 		"position": {\n\
+	 			"x": 306.2,\n\
+	 			"y": 100,\n\
+	 			"w": 180\n\
+	 		}\n\
+	 	}\n\
+ 	],\n\
+	"cartesianSpots": [\n\
+		[ 20, 200 ],\n\
+		[ 30, 202 ],\n\
+		[ 40, 199 ],\n\
+		[ 148, 104 ],\n\
+		[ 145, 95 ],\n\
+		[ 153, 105 ],\n\
+		[ 155, 98 ],\n\
+		[ 95, 135 ],\n\
+		[ 100, 129 ],\n\
+		[ 101, 140 ],\n\
+		[ 105, 132 ],\n\
+		[ 104, 137 ],\n\
+		[ 230, 2 ],\n\
+		[ 234, 4 ]\n\
+	],\n\
+	"robotsSpots": [\n\
+		[ 150, 100 ],\n\
+		[ 100, 135 ]\n\
+	]\n\
+}';
 
 	$scope.update = function() {
 		Hokuyo.onOrder($scope.from, $scope.name, JSON.parse($scope.data));
@@ -41,7 +86,7 @@ angular.module('app').service('Hokuyo', ['$rootScope', '$sce', 'Client',
 			main: null,
 			one: null,
 			two: null
-		}
+		};
 
 		this.init = function () {
 			Client.order(this.onOrder);
@@ -49,15 +94,18 @@ angular.module('app').service('Hokuyo', ['$rootScope', '$sce', 'Client',
 
 		this.onOrder = function (from, name, data) {
 			if($rootScope.act_page == 'hokuyo') {
-					if (name == 'hokuyo.polar_raw_data') {
-						if (data.hokuyo == "corner" && !!this.displays.one) {
-							this.displays.one.updatePolarSpots(data.polarSpots);
-						} else if (data.hokuyo == "enemy" && !!this.displays.two) {
-							this.displays.two.updatePolarSpots(data.polarSpots);
+				if (name == 'hokuyo.polar_raw_data') {
+					if (data.hokuyo == "corner" && !!this.displays.one) {
+						this.displays.one.updatePolarSpots(data.polarSpots);
+					} else if (data.hokuyo == "enemy" && !!this.displays.two) {
+						this.displays.two.updatePolarSpots(data.polarSpots);
 					}
-				} else if (name == 'hokuyo.robots'
+				} else if (name == 'lidar.all'
 					&& !!this.displays.main) {
-					this.displays.main.updateRobots(data.robots);
+					this.displays.main.updateAll(data.hokuyos, data.robotsSpots, data.cartesianSpots);
+				} else if (name == 'lidar.robots'
+					&& !!this.displays.main) {
+					// this.displays.main.updateRobots(data.robots);
 				}
 			}
 		}.bind(this);
