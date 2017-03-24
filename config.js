f215fc61bc84c1d2212325c3d4db1ede9427a99f
utcoupe-rpi <utcoupe@assos.utc.fr>
2017-03-24 23:07:52 +0000

    Change path to hokuyo exe

--- config.js
+++ config.js
@@ -8,5 +8,5 @@
 module.exports = {
 	"server": "127.0.0.1:3128",
 	//"server": "192.168.0.100:3128",
-	"hokuyo_command": "/root/coupe15/hokuyo/bin/hokuyo"
+	"hokuyo_command": "/home/pi/coupe17/hokuyo/build/bin/hokuyo"
 }
