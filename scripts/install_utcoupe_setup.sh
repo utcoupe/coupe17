adfdf81672d44f36433928dacd2a01fccc60823f
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-04-06 22:24:13 +0200

    Update install script, now the user can choose what he wanted to install

--- scripts/install_utcoupe_setup.sh
+++ scripts/install_utcoupe_setup.sh
@@ -3,45 +3,105 @@
 ### The goal of this script is to install all UTCoupe specific packages to have a working setup.
 ### This script is called automatically when you run a npm install.
 
-### First install the linux packages
+function green_echo () {
+	echo -e "\033[32m$1\033[0m"
+}
 
-sudo apt-get install git build-essential python cmake libboost-dev libsdl1.2-dev
+function red_echo () {
+	echo -e "\033[31m$1\033[0m"
+}
 
-# Check if it's a PC or a raspi
-if [ "$(uname -m)" == "x86_64" ]; then
-	sudo apt-get install libsdl1.2-dev nodejs npm nodejs-legacy linux-headers-$(uname -r)
-else
-	sudo apt-get install libsdl1.2-dev raspberrypi-kernel-headers
-	sudo apt-get remove npm nodejs nodejs-legacy
-	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-	sudo npm install npm@3.5.2 -g
-fi
+# Globals
+ARCH=$(uname -m)
 
-### Then setup the variable environment to taget the UTCoupe main folder
 
-if [ -z "$UTCOUPE_WORKSPACE" ]; then
-	echo "export UTCOUPE_WORKSPACE=$PWD" >> $HOME/.bashrc
-	# Set the UTCOUPE_WORKSPACE variable by hand the first time, because can't source bashrc
-	UTCOUPE_WORKSPACE=$PWD
-fi
+### Install the linux packages
+function apt_install() {
+	green_echo "Install missing packages..."
+	sudo apt-get install git build-essential python cmake libboost-dev libsdl1.2-dev
 
-### Then install the UTCoupe libraries
+	# Check if it's a PC or a raspi
+	if [ "$ARCH" = "x86_64" ]; then
+		green_echo "x86 architecture detected."
+		sudo apt-get install nodejs npm nodejs-legacy linux-headers-$(uname -r)
+	else
+		green_echo "Raspberry Pi system detected, remove previous npm installation to setup the used version."
+		sudo apt-get install raspberrypi-kernel-headers
+		sudo apt-get remove npm nodejs nodejs-legacy
+		curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+		sudo npm install npm@3.5.2 -g
+	fi
+}
+
+### Setup the variable environment to taget the UTCoupe main folder
+function env_setup() {
+	if [ -z "$UTCOUPE_WORKSPACE" ]; then
+		green_echo "Env variable is not set."
+		echo "export UTCOUPE_WORKSPACE=$PWD" >> $HOME/.bashrc
+		exec bash
+	fi
+}
+
+### Compile and install the UTCoupe libraries
 
 # URG library for the hokuyo
-cd $UTCOUPE_WORKSPACE/libs/urg-0.8.18
-./configure && make && sudo make install
+function compile_urg() {
+	cd $UTCOUPE_WORKSPACE/libs/urg-0.8.18
+	./configure && make && sudo make install
+}
 
 # Archer driver for 5 GHz wifi
-cd $UTCOUPE_WORKSPACE/libs/Archer_T1U_V1_150909/Driver
-sudo make && sudo make install
-#TODO add the ra0 interface in configuration files
+function compile_archer() {
+	cd $UTCOUPE_WORKSPACE/libs/Archer_T1U_V1_150909/Driver
+	sudo make && sudo make install
+	#TODO add the ra0 interface in configuration files
+}
 
 ### Then install the UTCoupe softwares
 
 # The pathfinding
-cd $UTCOUPE_WORKSPACE/pathfinding
-./make.sh
+function compile_pathfinding() {
+	cd $UTCOUPE_WORKSPACE/pathfinding
+	./make.sh
+}
 
 # The hokuyo
-cd $UTCOUPE_WORKSPACE/hokuyo
-./make.sh
+function compile_hokuyo() {
+	cd $UTCOUPE_WORKSPACE/hokuyo
+	./make.sh
+}
+
+function launch_script() {
+	env_setup
+	
+	printf "Install apt missing packets ? [Y/n]?"
+	read answer
+	if [ "$answer" = "" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+		install_apt
+	fi
+	
+	printf "Compile urg library (mandatory for hokuyo) ? [Y/n]?"
+	read answer
+	if [ "$answer" = "" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+		compile_urg
+	fi
+	
+	printf "Compile archer library (mandatory for 5 GHz usb wifi key) ? [Y/n]?"
+	read answer
+	if [ "$answer" = "" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+		compile_archer
+	fi
+	
+	printf "Compile UTCoupe exe (pathfinding and hokuyo) ? [Y/n]?"
+	read answer
+	if [ "$answer" = "" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+		compile_pathfinding
+		compile_hokuyo
+	fi
+}
+
+printf "Launch install script ? [Y/n]?"
+read answer
+if [ "$answer" = "" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+	launch_script
+fi
