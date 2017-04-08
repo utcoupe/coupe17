8817e3f5d265da5d56ddee2fffd0e0c130f23dd1
Mamal <eumael.desseaux@etu.utc.fr>
2017-04-08 16:27:50 +0200

    Update of the AsservSimu class

--- clients/Asserv/AsservReal.class.js
+++ clients/Asserv/AsservReal.class.js
@@ -14,8 +14,36 @@ const Asserv = require('Asserv.class.js');
  * @extends {clients/Asserv/Asserv.Asserv}
  */
 class AsservReal extends Asserv{
-	constructor(){
-		
+	constructor(sp, client, who, sendStatus, fifo){
+		super(client, who, fifo);
+		this.COMMANDS = require('./defineParser.js')('./arduino/asserv/protocol.h');
+		this.DETECT_SERIAL_TIMEOUT = 100; //ms, -1 to disable
+		this.ready = true;
+		this.sendStatus = sendStatus;
+		this.currentId = 0;
+		this.color = "yellow";
+
+		this.sp.on("data", function(data){
+			if(this.ready === false){
+				this.ready = true;
+				this.sendStatus();
+			}
+			this.parseCommand(data.toString());
+		}.bind(this));
+		this.sp.on("error", function(data){
+			this.ready = false;
+			this.sendStatus();
+			logger.debug("error", data.toString());
+		}.bind(this));
+		this.sp.on("close", function(data){
+			this.ready = false;
+			this.sendStatus();
+			logger.error("Serial port close");
+		}.bind(this));
+
+		setTimeout(function() {
+			this.getPos();
+		}.bind(this), 2000);
 	}
 }
 
