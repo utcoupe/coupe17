405fa3873bb486f342ed5087ca5c283c59334894
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-04-08 23:04:25 +0200

    Remove hokuyo stuff, not used anymore because directly in hokuyo client

--- config.js
+++ config.js
@@ -6,8 +6,6 @@
  */
 
 module.exports = {
-	"server": "127.0.0.1:3128",
-	//"server": "192.168.0.100:3128",
-	"hokuyo_command": "home/francois/dev/utcoupe/coupe17/bin/hokuyo",
-	"UTCOUPE_WORKSPACE": "/home/francois/dev/utcoupe/coupe17/"
+	"server": "127.0.0.1:3128"
+	//"server": "192.168.0.100:3128"
 }
