2a65ea208b4d07398d32eae76b56c45e332d373d
utcoupe-dev rpi3-3 <utcoupe@assos.utc.fr>
2017-04-15 18:11:29 +0000

    Startup script finally working...

--- scripts/utcoupe_hokuyo.sh
+++ scripts/utcoupe_hokuyo.sh
@@ -1,35 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 ### BEGIN INIT INFO
 # Provides:		utcoupe_hokuyo
-# Default-Start		2 3 4 5
-# Default-Stop		0 1 6
-# Short-Description:	Starts the utcoupe node client for the hokuyo
+# Required-Start:
+# Required-Stop:
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	UTCoupe node client for the hokuyo
 ### END INIT INFO
-#/etc/init.d/utcoupe_hokuyo
 
-#export PATH=$PATH:/usr/local/bin
-#export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
-PID_FILE=/var/log/utcoupe/utcoupe_hokuyo.pid
+PID_FILE_D=/var/log/utcoupe/utcoupe_hokuyo_deamon.pid
+PID_FILE_F=/var/log/utcoupe/utcoupe_hokuyo_forever.pid
 
-function start_hokuyo {
+start_hokuyo() {
 	echo -n "Starting the utcoupe_hokuyo node client..."
-	exec forever --sourceDir=$UTCOUPE_WORKSPACE/hokuyo -p $PID_FILE main.js
+#	start-stop-daemon -p $PID_FILE_D -m -b -u $USER -S --exec forever -- --sourceDir=$UTCOUPE_WORKSPACE/hokuyo -l /var/log/utcoupe/forever.log -p $PID_FILE_F main.js
+
+#	start-stop-daemon -p $PID_FILE_D -v -m -b --start --exec /usr/local/bin/node -- $UTCOUPE_WORKSPACE/hokuyo/main.js
+#	start-stop-daemon --start -m --pidfile $PID_FILE_D --background --startas /bin/bash -- -E -c "/usr/local/bin/node $UTCOUPE_WORKSPACE/hokuyo/main.js > /var/log/utcoupe/hokuyo_daemon.log 2>&1"
+
+	# Working !
+#	UTCOUPE_WORKSPACE=$UTCOUPE_WORKSPACE start-stop-daemon --start -m --pidfile $PID_FILE_D --startas /bin/bash -- -c "node $UTCOUPE_WORKSPACE/hokuyo/main.js > /var/log/utcoupe/hokuyo_daemon.log 2>&1"
+
+	UTCOUPE_WORKSPACE=$UTCOUPE_WORKSPACE start-stop-daemon --start -m --pidfile $PID_FILE_D --background --exec /usr/local/bin/node -- $UTCOUPE_WORKSPACE/hokuyo/main.js
+
+#	start-stop-daemon --start -m --pidfile $PID_FILE_D --background --startas node -- $UTCOUPE_WORKSPACE/hokuyo/main.js > /var/log/utcoupe/hokuyo_daemon.log 2>&1
+#	forever --sourceDir=$UTCOUPE_WORKSPACE/hokuyo -p $PID_FILE_F main.js
+#	su - $USER -c "$UTCOUPE_WORKSPACE/node_modules/forever/bin/forever --sourceDir=$UTCOUPE_WORKSPACE/hokuyo -p $PID_FILE_F main.js"
+#--> fail because fucking node is launching in sudo
 	echo "done."
 }
 
-function stop_hokuyo {
+stop_hokuyo() {
 	echo -n "Terminating the utcoupe_hokuyo node client...."
-	exec forever stop --sourceDir=$UTCOUPE_WORKSPACE/hokuyo main.js
+#	forever stop --sourceDir=$UTCOUPE_WORKSPACE/hokuyo main.js
+#	start-stop-daemon -K -v -p $PID_FILE_D --startas /bin/bash -- -c "node $UTCOUPE_WORKSPACE/hokuyo/main.sh"
+	start-stop-daemon -K -v -p $PID_FILE_D --exec /usr/local/bin/node
 	echo "done."
 }
 
-# Retrieves the UTCOUPE_WORKSPACE value
 [ -f /etc/default/utcoupe ] && . /etc/default/utcoupe
 if [ -z "$UTCOUPE_WORKSPACE" ] ;  then
   echo "UTCOUPE_WORKSPACE is not set, please set it in /etc/default/utcoupe" >&2
   exit 1
 fi
+
+#export PATH=$PATH:$UTCOUPE_WORKSPACE/node_modules/forever/bin
+#export NODE_PATH=$NODE_PATH:$UTCOUPE_WORKSPACE/node_module/forever/lib
 
 case "$1" in
 start)
