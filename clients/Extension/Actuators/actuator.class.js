03bca875c6754c21174bfaa515247c14649c9c8c
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-04-30 21:30:06 +0200

    Implement servo class and adapt actuator (git add clients/Extension/Actuators/* not tested yet)

--- clients/Extension/Actuators/actuator.class.js
+++ clients/Extension/Actuators/actuator.class.js
@@ -23,35 +23,40 @@ class Actuator {
         if (new.target === Abstract) {
             throw new TypeError("Cannot construct Abstract instances directly");
         }
-        if (this.parseCommand === undefined) {
+        if (parseCommand === undefined) {
             throw new TypeError("Must override parseCommand");
         }
 
         this.logger = this.Log4js.getLogger(actuator);
         this.ordersCallback = [];
         this.currentOrderId = 0;
-        this.actuatorCommands = [];
+        this.actuatorCommands = {};
 
         //todo AX12 use a serial ?
         //todo adapt serialport name to actuator
         // Serial port stuff, creation and binding callbacks
+        // Ready means that the serial port is open
         this.serialPortReady = false;
+        // Connected means that the node has started the device through serial port
+        this.serialPortConnected = false;
         this.serialPort = new SerialPort("/dev/ttyUSB0", {
             baudrate: 57600,
-            parser:sp.parsers.readline('\n')
+            parser:sp.parsers.readline("\n")
         });
         this.serialPort.on("data", function(data){
             if(this.serialPortReady === false){
                 this.serialPortReady = true;
-                this.sendStatus();
+                //todo
+                // this.sendStatus();
             }
-            this.parseCommand(data.toString());
+            parseCommand(data.toString());
         }.bind(this));
         this.serialPort.on("error", function(data){
             this.logger.debug("Serial port error : " + data.toString());
         });
         this.serialPort.on("close", function(){
             this.serialPortReady = false;
+            //todo
             // this.sendStatus();
             this.logger.error("Serial port close");
         }.bind(this));
@@ -59,28 +64,32 @@ class Actuator {
         //add the id at the second position, orderToSend is the string to send to the actuator
         function addOrderId(orderToSend) {
             this.currentOrderId++;
-            return (orderToSend.slice(0, 1) + this.currentOrderId + ';' + orderToSend.slice(2))
-        }
-
-        //call the callback corresponding to the received order id, stored in ordersCallback
-        function callOrderCallback(orderId, params) {
-            for (var index = 0; index < this.ordersCallback; index++) {
-                if (this.ordersCallback[index][0] == orderId) {
-                    // Call the callback
-                    this.ordersCallback[index][1](params);
-                    // Remove the line from the array
-                    this.ordersCallback.splice(index, 1);
-                }
-            }
+            return (orderToSend.slice(0, 1) + this.currentOrderId + ";" + orderToSend.slice(2))
         }
     }
 
+    //call the callback corresponding to the received order id, stored in ordersCallback
+    callOrderCallback(orderId, params) {
+    for (var index = 0; index < this.ordersCallback; index++) {
+        if (this.ordersCallback[index][0] == orderId) {
+            // Call the callback
+            this.ordersCallback[index][1](params);
+            // Remove the line from the array
+            this.ordersCallback.splice(index, 1);
+        }
+    }
+}
+
     sendOrder(orderType, servoId, callback) {
-        //todo ';' as protocol separator
-        var order = orderType + ';' + servoId + ';\n';
-        order = addOrderId(order);
-        this.ordersCallback.push([this.currentOrderId, callback]);
-        this.serialPort.write(order);
+        //todo ";" as protocol separator
+        if (this.serialPortConnected) {
+            var order = orderType + ";" + servoId + ";\n";
+            order = addOrderId(order);
+            this.ordersCallback.push([this.currentOrderId, callback]);
+            this.serialPort.write(order);
+        } else {
+            this.logger.error("SerialPort is not connected...");
+        }
     }
 
     //filename is the name + the path of the file
@@ -89,13 +98,15 @@ class Actuator {
             if(err == null) {
                 this.actuatorCommands = require('../../Shared/defineParser.js')(filename);
             } else {
-                this.logger.error('Try to parse parameter file ' + filename + ', but an error ocured : ' + err.code);
+                this.logger.error("Try to parse parameter file " + filename + ", but an error ocured : " + err.code);
             }
         })
     }
 
     // Pure virtual method to implement in children
-    //parseCommand(receivedcommand);
+    // parseCommand(receivedCommand) {
+    //
+    // }
 }
 
 module.exports = Actuator;
--- clients/Extension/Actuators/servo.class.js
+++ clients/Extension/Actuators/servo.class.js
@@ -7,7 +7,7 @@
 
 "use strict";
 
-const Actuator = require('actuator.class.js');
+const Actuator = require('./actuator.class.js');
 
 /**
  * Classe implémentant l'actuator pour le servo-moteur
@@ -17,8 +17,73 @@ const Actuator = require('actuator.class.js');
  */
 class Servo extends Actuator {
     constructor () {
-        //
+        super();
+        super.parseParameterFile(process.env.UTCOUPE_WORKSPACE + "arduino/common/others/protocol.h");
+    }
+
+    // Automatically called by the super class
+    parseCommand(receivedCommand) {
+        if (!super.serialPortConnected) {
+            //todo robot+"_others"
+            // If not connected, wait the ID of the arduino before doing something else
+            if (receivedCommand == "pr_others") {
+                super.sendOrder(super.actuatorCommands.START, 0, undefined);
+                super.serialPortConnected = true;
+            }
+        } else {
+            // Check if the received command is a debug string or a response from an order
+            //todo ";" as protocol separator
+            if (receivedCommand.indexOf(";") == 1) {
+                // It's an order response
+                //todo ";" as protocol separator
+                var splittedCommand = receivedCommand.split(";");
+                super.callOrderCallback(splittedCommand[0], splittedCommand.slice(0, 1));
+            } else {
+                // It's a debug string
+                super.logger("SERVO : " + receivedCommand);
+            }
+        }
+    }
+
+    // Orders used by the extension
+    moduleArmClose() {
+        super.sendOrder(super.actuatorCommands.SERVO_CLOSE, super.actuatorCommands.PR_MODULE_ARM, function(params){
+            //todo advertise IA
+        });
+    }
+
+    moduleArmOpen() {
+        super.sendOrder(super.actuatorCommands.SERVO_OPEN, super.actuatorCommands.PR_MODULE_ARM, function(params){
+            //todo advertise IA
+        });
+    }
+
+    //implicit rotate the module
+    moduleEngage() {
+        super.sendOrder(super.actuatorCommands.SERVO_CLOSE, super.actuatorCommands.PR_MODULE_DROP_R, function(params){
+            //todo advertise IA
+        });
+        super.sendOrder(super.actuatorCommands.SERVO_CLOSE, super.actuatorCommands.PR_MODULE_DROP_L, function(params){
+            //todo advertise IA
+        });
+        super.sendOrder(super.actuatorCommands.MODULE_ROTATE, super.actuatorCommands.PR_MODULE_ROTATE, function(params){
+            //todo advertise IA
+        });
+    }
+
+    moduleDrop() {
+        //todo modules--
+        super.sendOrder(super.actuatorCommands.SERVO_OPEN, super.actuatorCommands.PR_MODULE_DROP_R, function(params){
+            //todo advertise IA
+        });
+        super.sendOrder(super.actuatorCommands.SERVO_OPEN, super.actuatorCommands.PR_MODULE_DROP_L, function(params){
+            //todo advertise IA
+        });
     }
 }
 
-module.exports = Servo;
+//differentiate ready and connected
+// -> at first, arduino will send ID, waiting from the node send a START
+
+// Export an object, to make it unique in the system
+module.exports = Servo();
