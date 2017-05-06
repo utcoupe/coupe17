8d1147ec93b2eedb97add39da43920d42826ecc4
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-05-06 23:57:17 +0200

    Change radius from float to uint

--- pathfinding/src/lib/map.cpp
+++ pathfinding/src/lib/map.cpp
@@ -39,15 +39,16 @@ MAP::~MAP() {
     delete map_barrier;
 }
 
-void MAP::add_dynamic_circle(unsigned int x, unsigned int y, float f_r) {
-    unsigned int r = (unsigned int)ceil(f_r);
+void MAP::add_dynamic_circle(unsigned int x, unsigned int y, unsigned int f_r) {
+//    unsigned int r = (unsigned int)ceil(f_r);
+    unsigned int r = f_r;
     int r2 = r*r;
     for (unsigned int p_x = x - r; p_x <= x + r; p_x++) {
         if (p_x < 0 || p_x >= map_w) {
             continue;
         }
         //todo test pow integer versus std using double
-        int y_length = (int)ceil(sqrt((float)r2 - std::pow((float)(x - r + p_x), (float)2)));
+        int y_length = (int)ceil(sqrt((float)r2 - std::pow((float)(x - p_x), (float)2)));
         for (unsigned int p_y = y - y_length; p_y <= y + y_length; p_y++) {
             if (p_y < 0 || p_y >= map_h) {
                 continue;
