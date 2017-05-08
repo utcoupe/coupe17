7ce8a2fdc5d9d231bc14e7ff41f1b15c26af321f
Mindstan <mindstan@hotmail.fr>
2017-05-08 18:03:08 +0200

    Rewrote SocketClient : now is a class

--- server/socket_client.class.js
+++ server/socket_client.class.js
@@ -7,21 +7,23 @@
  * @see {@link server/socket_client.SocketClient}
  */
 
-module.exports = (function () {
-	"use strict";
-	var log4js = require('log4js');
-	var logger = log4js.getLogger('Client');
+"use strict";
 
-	/**
-	 * Create a socket for the clients
-	 * 
-	 * @exports server/socket_client.SocketClient
-	 * @constructor
-	 * @param {Object} params
-	 * @param {string} [params.server_ip=127.0.0.1:3128] Server IPv4 and port
-	 * @param {string} params.type Type of client
-	 */
-	function SocketClient(params) {
+var SocketIO_client = require('socket.io-client');
+const Log4js = require('log4js');
+
+/**
+ * logger
+*/
+var logger = Log4js.getLogger('Client');
+
+/**
+ * Socket permettant l'échange entre les clients et le serveur
+ * 
+ */
+class SocketClient {
+	constructor (params) {
+		
 		/**
 		 * Server IP
 		 * @type {string}
@@ -31,7 +33,7 @@ module.exports = (function () {
 		 * Socket du client
 		 * @type {socket.io-client}
 		 */
-		this.client = require('socket.io-client')('http://'+this.server_ip);
+		this.client = SocketIO_client('http://'+this.server_ip);
 		/**
 		 * callbacks
 		 * @type {Array<Function>}
@@ -39,6 +41,7 @@ module.exports = (function () {
 		this.callbacks = {};
 		/**
 		 * Variable permettant au client d'ignorer les échanges réseaux
+		 * @type {bool}
 		 */
 		this.muted = false;
 
@@ -49,7 +52,7 @@ module.exports = (function () {
 			logger.error("Missing client type.");
 
 		// When the client is connected to the server
-		this.client.on('connect', function(){
+		this.client.on('connect', () => {
 			logger.info('Client connected to server');
 			this.client.emit('type', {
 				type: this.type,
@@ -60,20 +63,20 @@ module.exports = (function () {
 			if(!!this.callbacks.connect)
 				this.callbacks.connect();
 			// this.client.emit('order', {to:'client2',text:'Hello!'});
-		}.bind(this));
+		});
 
 		// When the client is connected to the server
-		this.client.on('disconnect', function(){
+		this.client.on('disconnect', () => {
 			logger.error('Client disconnected from server');
-		}.bind(this));
+		});
 
 		// When the client receive log from the server
-		this.client.on('log', function(data){
+		this.client.on('log', (data) => {
 			logger.info('[Server log] '+data);
-		}.bind(this));
+		});
 
 		// When the client receive order from the server
-		this.client.on('order', function(data){
+		this.client.on('order', (data) => {
 			// logger.info('[Order to '+data.to+'] '+data.text);
 			// On n'autorise pas la réception de message si on est muet, sauf si c'est un start
 			if (this.muted && data.name != "start" && data.name != "kill")
@@ -84,32 +87,33 @@ module.exports = (function () {
 						this.callbacks.order(data.from, data.name, data.params || {});
 					else
 						logger.error("Order has no name ! : " + data);
-		}.bind(this));
+		});
 
 		// If after 500ms the client isn't connected, throw "server not found" error
-		setTimeout(function() {
+		setTimeout(() => {
 			if(this.client.disconnected)
 				this.errorServerNotFound();
-		}.bind(this), 500);
+		}, 500);
 	}
 
 	/**
 	 * connect
 	 * 
-	 * @param callback
+	 * @param {function} callback
 	 */
-	SocketClient.prototype.connect = function (callback) {
+	connect (callback) {
 		this.callbacks.connect = callback;
-	};
+	}
 
 	/**
 	 * order
 	 * 
-	 * @param callback
+	 * @param {function} callback
 	 */
-	SocketClient.prototype.order = function (callback) {
+	order (callback) {
 		this.callbacks.order = callback;
-	};
+	}
+
 	/**
 	 * Send parameters
 	 * 
@@ -117,7 +121,7 @@ module.exports = (function () {
 	 * @param {string} name
 	 * @param {Object} params
 	 */
-	SocketClient.prototype.send = function (to, name, params) {
+	send (to, name, params) {
 		// logger.debug('send %s to %s', name, to);
 		if (this.muted)
 			console.log('A client tried to send an order, but he is muted!');
@@ -128,7 +132,7 @@ module.exports = (function () {
 				params: params,
 				from: this.type
 			});
-	};
+	}
 
 	// Error functions
 	/**
@@ -136,35 +140,37 @@ module.exports = (function () {
 	 * 
 	 * @param {string} msg message to send
 	 */
-	SocketClient.prototype.throwError = function (msg) {
+	throwError (msg) {
 		logger.error(msg);
-	};
+	}
+
 	/**
 	 * Throw Server not found
 	 */
-	SocketClient.prototype.errorServerNotFound = function () {
+	errorServerNotFound () {
 		this.throwError('Server not found at '+this.server_ip+', please make sure the server is running.');
-	};
+	}
+
 	/**
 	 * Throw Server timed out
 	 */
-	SocketClient.prototype.errorServerTimeout = function () {
+	errorServerTimeout () {
 		this.throwError('Server timed out, please make sure the server is still running.');
-	};
+	}
 
 	/**
 	 * Désactive temporairement le client
 	 */
-	SocketClient.prototype.mute = function () {
+	mute () {
 		this.muted = true;
-	};
+	}
 
 	/**
 	 * Réactive le client
 	 */
-	SocketClient.prototype.unMute = function () {
+	unMute () {
 		this.muted = false;
 	}
+}
 
-	return SocketClient;
-})();
+module.exports = SocketClient;
