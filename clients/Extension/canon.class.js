bac29dacde66b7e6400567ba6120fc8a676cf818
Mindstan <mindstan@hotmail.fr>
2017-05-14 00:00:47 +0200

    Added new order for cannon --- Not using servos yet

--- clients/Extension/canon.class.js
+++ clients/Extension/canon.class.js
@@ -24,13 +24,47 @@ class Canon extends Extension {
         this.servos = servos;
     }
 
-    processFifoOrder (name, param) {
+    takeOrder(from, name, params) {
         this.logger.info ("Order received: " + name);
         switch (name) {
+            case "throw_balls":
+                this.fifo.order (() => {
+                    this.processFifoOrder("turn_on", params);
+                });
+                setTimeout(() => {
+                    this.fifo.order_in_progress (() => {
+                        this.processFifoOrder("turn_off", params);
+                    });
+                });
+                break;
+
+            case "engage_ball":
+            case "turn_off":
             case "turn_on":
+                this.fifo.newOrder(() => {
+                    this.processFifoOrder (name, params);
+                });
+                break;
+            
+            default:
+                this.logger.error ("Order " + name + "does not exist!");
+        }
+    }
+
+    processFifoOrder (name, param) {
+        this.logger.info ("Executing order: " + name);
+        switch (name) {
+            case "turn_on":
+                // TODO turn on motors
                 this.fifo.orderFinished();
                 break;
             case "turn_off":
+                // TODO turn off motors
+                this.fifo.orderFinished();
+                break;
+            
+            case "engage_ball":
+                // TODO something
                 this.fifo.orderFinished();
                 break;
 
