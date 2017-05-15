ee0b270af42add9c50cd9a179c16eb1fceea180a
clecuyer <cedric.lecuyer580@gmail.com>
2017-05-15 21:26:51 +0200

    Delete useless comment on robot and pr

--- clients/robot/pr.js
+++ clients/robot/pr.js
@@ -10,8 +10,6 @@
 
 //Préciser ce qu'attends le robot avant de commencer par un log
 
-//TODO Find a way to replace detect
-
 const Robot = require('./robot');
 const UnitGrabber = require('../extension/unitgrabber');
 const BaseConstructor = require('../extension/baseconstructor');
@@ -31,7 +29,7 @@ class Tibot extends Robot{
 		this.baseConstructor = new BaseConstructor();
 	}
 
-	
+
 	// called by start
 	openExtensions () {
         this.servo = this.factory.createObject("servo");
@@ -43,7 +41,7 @@ class Tibot extends Robot{
         this.unitGrabber.start(actuators);
         this.baseConstructor.start(actuators);
 	}
-	
+
 	// called by stop and exit
 	closeExtensions () {
         this.unitGrabber.stop();
