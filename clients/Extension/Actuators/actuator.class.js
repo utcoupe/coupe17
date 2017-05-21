85a2373cd13a1698dd9042ae50b5e88ffd17b476
mmxm <lesecq.f@gmail.com>
2017-05-21 23:57:41 +0200

    merge

---
+++ clients/Extension/Actuators/actuator.class.js
@@ -0,0 +1,21 @@
+/**
+ * Module définissant un actuator abstrait
+ * 
+ * @module clients/Extension/Actuators/actuator
+ */
+
+"use strict";
+
+/**
+ * Classe abstraite représentant un actuator
+ * 
+ * @class Actuator
+ * @memberof module:clients/Extension/Actuators/actuator
+ */
+class Actuator {
+    constructor () {
+        //
+    }
+}
+
+module.exports = Actuator;
---
+++ clients/Robot/tibot.class.js
@@ -0,0 +1,25 @@
+/**
+ * Module du petit robot
+ * 
+ * @module clients/Robot/tibot
+ * @requires module:clients/Robot/robot
+ */
+
+"use strict";
+
+const Robot = require('robot.class.js');
+
+/**
+ * Petit Robot
+ * 
+ * @class Grobot
+ * @memberof module:clients/Robot/tibot
+ * @extends {clients/Robot/robot.Robot}
+ */
+class Tibot extends Robot{
+	constructor(){
+		
+	}
+}
+
+module.exports = Tibot;
