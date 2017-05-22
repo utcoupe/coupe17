9d1a47ed05e2e981085a67773895b2819f32bd10
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-05-22 20:00:09 +0200

    Adapt gr asserv parameters

--- arduino/gr/asserv/parameters.h
+++ arduino/gr/asserv/parameters.h
@@ -8,7 +8,7 @@
 
 #define BAUDRATE 57600
 #define SERIAL_TYPE SERIAL_8N1
-#define ARDUINO_ID "grobot_asserv"
+#define ARDUINO_ID "gr_asserv"
 #define DEBUG_TARGET_SPEED 0
 #define DEBUG_MAINLOOP 0
 //todo add an enum with the different debug levels available
@@ -34,14 +34,14 @@
 
 #define ENCODER_EVAL 2
 
-#define USE_SHARP 1
+#define USE_SHARP 0
 #define EMERGENCY_STOP_DISTANCE 0.32 // m
 #define EMERGENCY_WAIT_TIME 30 // seconds
 #define EMERGENCY_SLOW_GO_RATIO 0.5 // spd = 0.3*max_spd in slow_go mode
 
 #define HZ 100
 #define DT (1.0/HZ)
-#define AUTO_STATUS_HZ 100 // must be a divider a HZ or 0 to disable
+#define AUTO_STATUS_HZ 10 // must be a divider a HZ or 0 to disable
 
 #define SPD_MAX 500 //mm/s
 #define ACC_MAX 1000  //mm/s2
@@ -50,9 +50,9 @@
 
 #define ENC_RESOLUTION 1024 //resolution du codeur
 
-#define ENC_LEFT_RADIUS 20.02 //REGLE PAR TEST - rayon de la roue codeuse
-#define ENC_RIGHT_RADIUS 20.02 //REGLE PAR TEST - rayon de la roue codeuse
-#define ENTRAXE_ENC 200.0 //REGLE PAR TES - Distance entre chaque roue codeuse en mm
+#define ENC_LEFT_RADIUS 31.38 //20.02 //REGLE PAR TEST - rayon de la roue codeuse
+#define ENC_RIGHT_RADIUS 31.38 //20.02 //REGLE PAR TEST - rayon de la roue codeuse
+#define ENTRAXE_ENC 307.0 //200.0 //REGLE PAR TES - Distance entre chaque roue codeuse en mm
 
 #define ERROR_ANGLE 0.015 //erreur en angle(radians) maximale pour considérer l'objectif comme atteint
 #define ERROR_POS 5 // erreur en position (mm)  maximale pour considérer l'objectif comme atteint
@@ -60,9 +60,9 @@
 
 #define CONE_ALIGNEMENT 100 // NEVER
 
-#define PID_P 1.5
-#define PID_I 30
-#define PID_D 5
+#define PID_P 1 //1.5
+#define PID_I 0 //30
+#define PID_D 0 //5
 #define PID_BIAS 0
 
 #define LEFT_P (PID_P)
