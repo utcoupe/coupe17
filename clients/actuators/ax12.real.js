92ee54f6494cb6ea7a03519b62946bda1ce3f072
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-05-22 16:57:33 +0200

    Change orders for grabber

--- clients/actuators/ax12.real.js
+++ clients/actuators/ax12.real.js
@@ -95,7 +95,7 @@ class Ax12Real extends Ax12 {
 
     openGrabber(callback) {
         if (this.stdStreamConnected) {
-            this.ordersSerial.sendOrder(this.actuatorCommands.AX12_INIT, [this.actuatorCommands.PR_MODULE_GRABBER], callback);
+            this.ordersSerial.sendOrder(this.actuatorCommands.AX12_ONE, [this.actuatorCommands.PR_MODULE_GRABBER], callback);
         } else {
             this.logger.error("TODO: AX12 real openGrabber()");
             // this.logger.error("Serial port not connected...");
@@ -104,7 +104,7 @@ class Ax12Real extends Ax12 {
 
     closeGrabber(callback) {
         if (this.stdStreamConnected) {
-            this.ordersSerial.sendOrder(this.actuatorCommands.AX12_ONE, [this.actuatorCommands.PR_MODULE_GRABBER], callback);
+            this.ordersSerial.sendOrder(this.actuatorCommands.AX12_INIT, [this.actuatorCommands.PR_MODULE_GRABBER], callback);
         } else {
             this.logger.error("TODO: AX12 real closeGrabber()");
             // this.logger.error("Serial port not connected...");
