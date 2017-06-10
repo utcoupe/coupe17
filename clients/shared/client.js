ca2cc6b6d8f273ec5ca256131b219c771703d930
Mindstan <mindstan@hotmail.fr>
2017-06-10 16:42:45 +0200

    Changed directory address and added more doc

--- clients/shared/client.js
+++ clients/shared/client.js
@@ -1,7 +1,7 @@
 /**
  * Module du client de base
  * 
- * @module clients/client
+ * @module clients/shared/client
  * @requires module:server/socket_client
  * @requires module:config
  * @requires module:log4js
@@ -16,7 +16,7 @@ const SocketClient = require('../../server/socket_client.class.js');
 /**
  * Client abstrait
  * 
- * @memberof module:clients/client
+ * @memberof module:clients/shared/client
  */
 class Client {
 	/**
@@ -41,24 +41,46 @@ class Client {
         this.client.order(this.takeOrder.bind(this));
 	}
 
+	/**
+	 * Takes an order
+	 * 
+	 * @param {String} from Sender of the order
+	 * @param {String} name Name of the order
+	 * @param {Object} [param] Parameters of the order
+	 */
     takeOrder (from, name, param) {
         throw new TypeError("client:takeOrder is abstract !");
     }
 
+	/**
+	 * Sends data to IA (shortcut). 
+	 * 
+	 * @param {String} name Name of the order
+	 * @param {Object} [params] Parameters of the order
+	 */
 	sendDataToIA(name, params){
 		this.client.send('ia', name, params);
 	}
 
+	/**
+	 * Allows the client to communicate
+	 */
 	start(){
 		this.client.unMute();
 	}
 
+	/**
+	 * Prohibits the client to communicate
+	 */
 	stop(){
 		//todo stop the socket client
 		this.client.mute();
         this.logger.info(this.clientName + " has stopped.");
 	}
 
+	/**
+	 * Sends the status
+	 */
 	status(){
 
 	}
