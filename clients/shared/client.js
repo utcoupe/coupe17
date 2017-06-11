5ebfbd15dbad942b572ca298afe06efa0818fc24
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-06-11 18:06:44 +0200

    Autoident + reformat code of client.js

--- clients/shared/client.js
+++ clients/shared/client.js
@@ -35,18 +35,18 @@ class Client {
      * @class Client
      * @param clientName {string}   Name of the client, used to route messages
      */
-	constructor(clientName){
+    constructor(clientName) {
         this.logger = Log4js.getLogger(clientName);
         this.clientName = clientName;
         this.parser = null;
-		var server = CONFIG.server;
+        var server = CONFIG.server;
         this.client = new SocketClient({
             server_ip: server,
             type: clientName
         });
         // Bind the callback function, which has to be overloaded
         this.client.order(this.takeOrder.bind(this));
-	}
+    }
 
     /**
      * Abstract callback function called when a client receives a message.
@@ -57,7 +57,7 @@ class Client {
      * @param name {string} Name of the order
      * @param param {json}  Parameters of the order
      */
-    takeOrder (from, name, param) {
+    takeOrder(from, name, param) {
         throw new TypeError("client:takeOrder is abstract !");
     }
 
@@ -67,28 +67,28 @@ class Client {
      * @param name {string} Name of the order
      * @param params {json} Parameters of the order
      */
-	sendDataToIA(name, params){
-		this.client.send('ia', name, params);
-	}
+    sendDataToIA(name, params) {
+        this.client.send('ia', name, params);
+    }
 
     /**
      * Start the client.
      * When a client has started, it can send and receive messages.
      * @protected
      */
-	start(){
-		this.client.unMute();
-	}
+    start() {
+        this.client.unMute();
+    }
 
     /**
      * Stop a client
      * When a client is stopped, it can't send and receive messages, but the client is still alive.
      * @protected
      */
-	stop(){
-		this.client.mute();
+    stop() {
+        this.client.mute();
         this.logger.info(this.clientName + " has stopped.");
-	}
+    }
 }
 
 module.exports = Client;
