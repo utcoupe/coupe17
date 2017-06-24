13ff8966fd3f0eb8bac063a91a1e856a4f26fb8c
Thomas Fuhrmann <thomas.fuhrmann@hds.utc.fr>
2017-06-24 23:00:00 +0200

    Add OrdersSerial and OrdersProcess documentation

--- clients/shared/orders.process.js
+++ clients/shared/orders.process.js
@@ -1,13 +1,30 @@
 /**
- * Created by tfuhrman on 13/05/17.
+ *  @file       Describes the orders process module.
+ *  @date       13/05/17
+ *  @module     clients/shared/ordersprocess
+ *  @copyright  Copyright (c) 2017 UTCoupe All rights reserved.
+ *  @licence    See licence.txt file
  */
 
 "use strict";
 
+/**
+ * Need the mother class for inheritance
+ * @type {OrdersManager}
+ */
 const OrdersManager = require('./orders.manager.js');
 
-//orders for inter process communication
+/**
+ * OrdersProcess is an inherited class from OrdersManager, dealing with standard process communication (stdin and stdout).
+ * For more information about the behaviour of OrdersManager, see OrdersManager class.
+ * @augments OrdersManager
+ */
 class OrdersProcess extends OrdersManager {
+    /**
+     * Constructor of OrdersProcess, instantiate an OrdersManager class dealing with standard process communication (stdin and stdout).
+     * For more details, see the OrdersManager constructor.
+     * @param {any} communicationLine       The communication stream, has to be a ByLine object (to read and write on stin / stdout).
+     */
     constructor(communicationLine) {
         super(communicationLine);
 
@@ -17,6 +34,12 @@ class OrdersProcess extends OrdersManager {
         }.bind(this));
     }
 
+    /**
+     * This method sends the order parameter through a standard process communication line.
+     * @override
+     * @protected
+     * @param {string} order    The formatted order to send
+     */
     comLineSend(order) {
         this.comLine.in.write(order);
     }
